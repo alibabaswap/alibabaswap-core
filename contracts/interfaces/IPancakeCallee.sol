fd79afb53f9ae4125a152d0c9a57fe87de7ee965
alibabaswap <85833509+alibabaswap@users.noreply.github.com>
2021-06-13 16:34:58 +0300

    Update IPancakeCallee.sol

--- contracts/interfaces/IPancakeCallee.sol
+++ contracts/interfaces/IPancakeCallee.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0;
 
-interface IPancakeCallee {
-    function pancakeCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
+interface IAlibabaCallee {
+    function AlibabaCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
